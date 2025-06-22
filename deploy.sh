2ff7e6c5efd8fd62e6dbc38997636d56556238c0
kamakarter <kamil.zag74@gmail.com>
2025-06-22 18:34:52 +0300

    add deploy file

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+echo 'Deploying..'
+
+git pull origin main
+
+php8.2 artisan down
+
+php8.2 composer.phar install --no-dev --optimize-autoloader
+
+php8.2 artisan migrate --force
+
+php8.2 artisan config:cache
+
+php8.2 artisan route:cache
+
+php8.2 artisan view:cache
+
+php8.2 artisan up
+
+echo 'Deployed successfully'
+
+
+
